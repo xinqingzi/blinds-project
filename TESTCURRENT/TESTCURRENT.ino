e439047443650d223046e25c4dfe0396ce324821
xinqingzi <104529766+xinqingzi@users.noreply.github.com>
2024-09-21 22:04:56 -0700

    add limit witch
    add 3 more output pin to control pulling down with limit switch.

--- TESTCURRENT/TESTCURRENT.ino
+++ TESTCURRENT/TESTCURRENT.ino
@@ -4,15 +4,20 @@ const float referenceVoltage = 5.0; // Reference voltage of Arduino
 const int sensorZero = 512; // Midpoint of 10-bit ADC (for 5V reference)
 const float sensitivity = 0.185; // Sensitivity for ACS712 5A version (in V/A)
 
-const int IN1 = 13;
+const int IN1 = 13;  // going up
 const int IN2 = 12;
 const int ENB = 11;
-float pcurrent = 1.5;
+const int IN3 = 7;   // going down
+const int IN4 = 8;
+const int ENA = 9;
 
 void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
   Serial.begin(9600); // Initialize serial communication
 }
 
@@ -40,12 +45,12 @@ void loop() {
 
     if (command == '1') {
       //startTime = millis();  // Record start time
-      motor(350);  // Set motor speed to 50 (clockwise)
+      motor(350);  // Set motor speed to 350 (clockwise)
       Serial.println("Clockwise with speed of 250");
     } 
     else if (command == '2') {
       //startTime = millis();  // Record start time
-      motor(-350);  // Set motor speed to 50 (clockwise)
+      motor(-100);  // Set motor speed to 100 (clockwise)
       Serial.println("counterclockwise with speed of 250");
     }
     else if (command == '0') {
@@ -57,22 +62,23 @@ void loop() {
       //Serial.println(" ms");
     }
   }
-    pcurrent = current;
 }
 
 void motor(int speed) {
   analogWrite(ENB, abs(speed));
-   // Set speed using PWM
   if (speed > 0) { 
+    analogWrite(ENB, abs(speed));   // Set speed using PWM
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
   } else if (speed < 0) {
-    analogWrite(ENB, abs(speed)); 
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
+    analogWrite(ENA, abs(speed));    // Set speed using PWM
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
   } else {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
   }
 }
 
